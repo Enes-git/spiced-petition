4a77248bf02bf11f0ebd31b04e27b8399be9fc1c
enes <enes.3.ergun@gmail.com>
2021-02-18 09:23:27 +0100

    only petition page works

--- db.js
+++ db.js
@@ -3,22 +3,22 @@ const db = spicedPg('postgres:postgres:postgres@localhost:5432/petition');
 
 // selecting a total number of signers
 module.exports.totalSigners = () => {
-    const q = `SELECT * FROM petition`;
+    const q = `SELECT COUNT(*) FROM petition`;
     return db.query(q);
 };
 
 // inserting the user's signature and name
-module.exports.addSigner = (firstname, lastname, signature) => {
+module.exports.addSigner = (first, last, signature) => {
     const q = `
-    INSERT INTO petition(firstname, lastname, signature)
+    INSERT INTO petition (first, last, signature)
     VALUES ($1, $2, $3)
     RETURNING id`;
-    const params = [firstname, lastname, signature];
+    const params = [first, last, signature];
     return db.query(q, params);
 };
 
 // selecting first and last names of every signer
 module.exports.signerName = () => {
-    const q = `SELECT firstname, lastname FROM petition`;
+    const q = `SELECT first, last FROM petition`;
     return db.query(q);
 };
--- server.js
+++ server.js
@@ -1,46 +1,84 @@
 const db = require('./db');
+const hb = require('express-handlebars');
+const cookieSession = require('cookie-session');
 const express = require('express');
+
 const app = express();
 
-console.log('db :>> ', db);
+// ====== handlebars template engine =================
+app.engine('handlebars', hb());
+app.set('view engine', 'handlebars');
 
-// GET REQUEST TO /petition
-app.get('/petition', (req, res) => {
-    if (cookie) {
-        // HOW TO SET A COOKIE
-        res.redirect('/petition/thanks');
+// =========== MIDDLEWARES ============================
+//  cookie session middleware
+app.use(
+    cookieSession({
+        secret: `Hello underworld!`,
+        maxAge: 1000 * 60 * 60 * 24 * 14,
+    })
+);
+
+// middleware for post requests
+app.use(express.urlencoded({ extended: false }));
+
+//  serving the static files
+app.use(express.static('./public'));
+
+//  user check with cookie
+app.use((req, res, next) => {
+    if (req.session.signatureId) {
+        req.url != '/petition' ? next() : res.redirect('/thanks');
     } else {
-        res.render('/petition', { layout: 'main' });
+        req.url == '/petition' ? next() : res.redirect('/petition'); // can i get rid of this conditional and just redirect?????????????????
     }
 });
 
-// POST REQUEST TO /petition
+// ==================== REQUESTS ========================
+//  GET request to "/"
+app.get('/', (req, res) => {
+    res.redirect('/petition');
+});
+
+//  GET request to "/petition"
+app.get('/petition', (req, res) => {
+    res.render('petition', { layout: 'main' });
+});
+
+//  POST request to "/petition"
 app.post('/petition', (req, res) => {
     // user entered info from the form
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
-    const signature = req.body.sig_id;
+    const signature = req.body.signature;
 
     // adding user infos to petition database
-    db.addSigner(firstname, lastname, signature);
+    db.addSigner(firstname, lastname, signature)
+        .then(({ rows }) => {
+            // console.log('rows :>> ', rows);
+            // redirecting to thanks page and setting cookie
+            req.session.signatureId = rows[0].id;
+            res.redirect('/thanks');
+        })
+        .catch((err) => {
+            console.log('err >> ', err);
+            res.render('petition', { layout: 'main' }); //??????? HOW TO PASS AN ERR MSG ??????
+        });
 });
 
-// GET REQUEST TO /thanks
-app.get('/petition/thanks', (req, res) => {
-    if (cookie) {
-        res.render('/petition/thanks', { layout: 'main' });
-    } else {
-        res.redirect('/petition');
-    }
+//  GET request to "/thanks"
+app.get('/thanks', (req, res) => {
+    // haven't I checked the condition in my middleware before requests?????????????????????????????????????????????????
+    res.render('thanks', { layout: 'main' });
 });
 
-// GET REQUEST FOR /signers
-app.get('/petition/signers', (req, res) => {
-    if (cookie) {
-        db.signerName(); //HOW TO PASS THEM TO HANDLEBARS
-    } else {
-        res.redirect('/petition');
-    }
+//  GET request to "/signers"
+app.get('/signers', (req, res) => {
+    db.signerName()
+        .then(({ rows }) => {
+            res.render('signers', { layout: 'main', rows });
+        })
+        .catch((err) => console.log('err :>> ', err));
+    res.redirect('/petition');
 });
 
 app.listen(8080, () => console.log("I'm all ears!"));
